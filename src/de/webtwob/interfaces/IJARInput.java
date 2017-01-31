637c8ccb451afd47f41ebebb22dabe330c7c4fd2
BB20101997 <blessmann-bennet@t-online.de>
2017-02-01 00:44:53 +0100

    now extend the IJARLinkable interface

--- src/de/webtwob/interfaces/IJARInput.java
+++ src/de/webtwob/interfaces/IJARInput.java
@@ -3,8 +3,4 @@ package de.webtwob.interfaces;
 /**
  * Created by BB20101997 on 31. Jan. 2017.
  */
-public interface IJARInput {
-
-	void linkModel(IJARModel ijarm);
-
-}
+public interface IJARInput extends IJARLinkable{}
