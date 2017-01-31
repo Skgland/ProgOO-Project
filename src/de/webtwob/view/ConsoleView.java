ecff84a9ae569a1683ba1e7a5cf3712fe2b36498
BB20101997 <blessmann-bennet@t-online.de>
2017-02-01 00:46:19 +0100

    started working on a  console view

---
+++ src/de/webtwob/view/ConsoleView.java
@@ -0,0 +1,18 @@
+package de.webtwob.view;
+
+import de.webtwob.interfaces.IJARModel;
+import de.webtwob.interfaces.IJARView;
+
+/**
+ * Created by BB20101997 on 31. Jan. 2017.
+ */
+public class ConsoleView implements IJARView {
+
+	private IJARModel model;
+
+	@Override
+	public void linkModel(IJARModel ijarm) {
+		model = ijarm;
+	}
+
+}
