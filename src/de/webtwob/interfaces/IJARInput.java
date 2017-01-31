0a4064dba3cce45ae110a0b805d0d8851a9c6416
BB20101997 <blessmann-bennet@t-online.de>
2017-01-31 16:24:58 +0100

    - removed user specific settings + added interfaces for model, input and view

---
+++ src/de/webtwob/interfaces/IJARInput.java
@@ -0,0 +1,6 @@
+package de.webtwob.interfaces;
+
+/**
+ * Created by BB20101997 on 31. Jan. 2017.
+ */
+public class IJARInput {}
---
+++ src/de/webtwob/interfaces/IJARModel.java
@@ -0,0 +1,6 @@
+package de.webtwob;
+
+/**
+ * Created by BB20101997 on 31. Jan. 2017.
+ */
+public interface IJARModel {}
