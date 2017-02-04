d16c0cc41fa8b13a5b65acebe0aff872392c5172
Bennet <bb-github@t-online.de>
2017-02-04 22:56:02 +0100

    implemented updateView

--- test/de/webtwob/TestModel.java
+++ test/de/webtwob/TestModel.java
@@ -6,6 +6,7 @@ import de.webtwob.interfaces.IJARView;
 import de.webtwob.interfaces.IMenuEntry;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by BB20101997 on 02. Feb. 2017.
@@ -17,23 +18,26 @@ public class TestModel implements IJARModel {
 	public Mode mode = Mode.GAME;
 
 	private ArrayList<IJARInput> inputs = new ArrayList<>();
-	private ArrayList<IJARView> views = new ArrayList<>();
+	private ArrayList<IJARView>  views  = new ArrayList<>();
 
 	@Override
 	public void addView(IJARView ijarv) {
+
 		ijarv.linkModel(this);
-			views.add(ijarv);
+		views.add(ijarv);
 	}
 
 	@Override
 	public void addInput(IJARInput ijari) {
+
 		ijari.linkModel(this);
 		inputs.add(ijari);
 	}
 
 	@Override
 	public void removeInput(IJARInput ijari) {
-		if(inputs.remove(ijari)){
+
+		if (inputs.remove(ijari)) {
 			ijari.stop();
 		}
 
@@ -41,12 +45,14 @@ public class TestModel implements IJARModel {
 
 	@Override
 	public IJARModel.Mode getMode() {
+
 		return mode;
 	}
 
 	@Override
 	public void jump() {
-		if(mode == Mode.GAME) {
+
+		if (mode == Mode.GAME) {
 			System.out.println("Jump");
 			jump = true;
 		}
@@ -54,20 +60,23 @@ public class TestModel implements IJARModel {
 
 	@Override
 	public void setSneaking(boolean bool) {
+
 		sneak = bool;
-		if(mode == Mode.GAME) {
+		if (mode == Mode.GAME) {
 			System.out.println(bool ? "Sneaking" : "Not Sneaking");
 		}
 	}
 
 	@Override
 	public boolean isSneaking() {
+
 		return sneak && mode == Mode.GAME;
 	}
 
 	@Override
 	public void pause() {
-		if(mode == Mode.GAME) {
+
+		if (mode == Mode.GAME) {
 			mode = Mode.MENU;
 			System.out.println("Pause");
 		} else {
@@ -78,7 +87,8 @@ public class TestModel implements IJARModel {
 
 	@Override
 	public void doSelect() {
-		if(mode == Mode.MENU) {
+
+		if (mode == Mode.MENU) {
 			System.out.println("Selected");
 		}
 	}
@@ -90,52 +100,66 @@ public class TestModel implements IJARModel {
 
 	@Override
 	public void up() {
-		if(mode == Mode.MENU) {
+
+		if (mode == Mode.MENU) {
 			System.out.println("UP");
 		}
 	}
 
 	@Override
 	public void down() {
-		if(mode == Mode.MENU) {
+
+		if (mode == Mode.MENU) {
 			System.out.println("DOWN");
 		}
 
 	}
 
 	@Override
-	public IMenuEntry[] getMenuEntries() {
-		return new IMenuEntry[0];
+	public List<IMenuEntry> getMenuEntries() {
+
+		return new ArrayList<>();
 	}
 
 	@Override
 	public int getSelectedIndex() {
+
 		return 0;
+	}
+	@Override
+	public void updateViews() {
+
+		for (IJARView view : views) {
+			view.forceUpdate();
+		}
 	}
 
 	@Override
 	public void start() {
-		for(IJARView ijarView : views) {
+
+		for (IJARView ijarView : views) {
 			ijarView.start();
 		}
-		for(IJARInput ijarInput : inputs) {
+		for (IJARInput ijarInput : inputs) {
 			ijarInput.start();
 		}
 	}
 
 	@Override
 	public void stop() {
-		for(IJARInput ijarInput : inputs) {
+
+		for (IJARInput ijarInput : inputs) {
 			ijarInput.stop();
 		}
-		for(IJARView ijarView : views) {
+		for (IJARView ijarView : views) {
 			ijarView.stop();
 		}
 	}
 
 	@Override
 	public String toString() {
+
 		return "[TestModel]:\n" +
-				       "\t[Sneaking]: "+sneak+"\n";
+				"\t[Sneaking]: " + sneak + "\n";
 	}
 }
