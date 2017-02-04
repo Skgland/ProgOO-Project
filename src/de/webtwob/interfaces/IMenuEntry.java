edd131b6342dea7d6e305562f1b68dcac7d53107
Bennet <bb-github@t-online.de>
2017-02-04 22:53:06 +0100

    + a function to enable/disable Entries

--- src/de/webtwob/interfaces/IMenuEntry.java
+++ src/de/webtwob/interfaces/IMenuEntry.java
@@ -7,6 +7,7 @@ public interface IMenuEntry {
 
 	String getText();
 	boolean isActive();
+	void setActive(boolean b);
 	void executeAction();
 	default String getValue(){return null;}
 	default void setValue(String update){}
