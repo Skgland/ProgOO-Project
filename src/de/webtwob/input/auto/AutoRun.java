9fb966de0612a1197b919e514b46fe9278c3a138
Bennet <bb-github@t-online.de>
2017-02-27 20:25:25 +0100

    should now prevent some race conditions when starting and stopping

--- src/de/webtwob/input/auto/AutoRun.java
+++ src/de/webtwob/input/auto/AutoRun.java
@@ -12,74 +12,35 @@ public class AutoRun implements IJARInput {
 
 	private boolean enabled = false;
 	private IJARModel model;
+	private boolean   running;
 	private final Runnable run = this::marathon;
-	private boolean running;
-	private Thread  runner;
+	private Thread runner;
 
 	@Override
 	public void linkModel(IJARModel ijarm) {
-		if(enabled && model == null && ijarm != null) {
-			synchronized(run) {
+
+		if (enabled && model == null && ijarm != null) {
+			synchronized (run) {
 				run.notifyAll();
 			}
 		}
 		model = ijarm;
 	}
-
-	@Override
-	public void setEnabled(boolean enable) {
-		if(!enabled && enable && model != null) {
-			synchronized(run) {
-				run.notifyAll();
-			}
-		}
-		enabled = enable;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	@Override
-	public void start() {
-		if(runner == null) {
-			synchronized(run) {
-				running = true;
-				runner = new Thread(run);
-				runner.start();
-			}
-		}
-	}
-
-	@Override
-	public void stop() {
-		if(runner != null) {
-			synchronized(run) {
-				running = false;
-				try {
-					runner.join();
-				}
-				catch(final InterruptedException ignore) {
-				}
-				runner = null;
-			}
-		}
-	}
-
 	private void marathon() {
-		while(running) {
-			if(model != null && enabled) {
+
+		while (running) {
+			if (model != null && enabled) {
 				Rectangle[] rectangles = model.getHurdles();
 				boolean     jump       = false;
 				boolean     sneak      = false;
-				if(rectangles[1] != null && rectangles[1].getY() != 0) {
+				if (rectangles[1] != null && rectangles[1].getY() != 0) {
 					sneak = true;
 				}
-				inner: for(int i = 2; i < 7; i++) {
-					if(rectangles[i] != null) {
-						if(rectangles[i].getY() == 0) {
-							if(!sneak) {
+				inner:
+				for (int i = 2; i < 7; i++) {
+					if (rectangles[i] != null) {
+						if (rectangles[i].getY() == 0) {
+							if (!sneak) {
 								jump = true;
 							}
 						} else {
@@ -89,24 +50,71 @@ public class AutoRun implements IJARInput {
 					}
 				}
 
-				if(sneak!=model.isSneaking()){
+				if (sneak != model.isSneaking()) {
 					model.setSneaking(sneak);
 				}
-				if(jump) { model.jump(); }
+				if (jump) {
+					model.jump();
+				}
 			} else {
-				synchronized(run) {
+				synchronized (run) {
 					try {
 						run.wait();
+					} catch (final InterruptedException ignore) {
 					}
-					catch(final InterruptedException ignore) {
-					}
+				}
+			}
+		}
+	}
+	@Override
+	public String toString() {
+
+		return "[Auto Run]";
+	}	@Override
+	public void setEnabled(boolean enable) {
+
+		if (!enabled && enable && model != null) {
+			synchronized (run) {
+				run.notifyAll();
+			}
+		}
+		enabled = enable;
+	}
+
+	@Override
+	public boolean isEnabled() {
+
+		return enabled;
+	}
+
+	@Override
+	public void start() {
+
+		if (runner == null) {
+			synchronized (run) {
+				if (runner == null) {
+					running = true;
+					runner = new Thread(run);
+					runner.start();
 				}
 			}
 		}
 	}
 
 	@Override
-	public String toString() {
-		return "[Auto Run]";
+	public void stop() {
+		if (runner != null) {
+			synchronized (run) {
+				if (runner != null) {
+					running = false;
+					try {
+						runner.join();
+					} catch (final InterruptedException ignore) {
+					}
+					runner = null;
+				}
+			}
+		}
 	}
+
 }
