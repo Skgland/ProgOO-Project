1dec4fc4bf8034390cc229c49556958248bb1f1e
BB20101997 <bb-github@t-online.de>
2017-03-02 17:42:37 +0100

    rewritten to use lwjgl

--- src/de/webtwob/input/controller/XBox360Const.java
+++ src/de/webtwob/input/controller/XBox360Const.java
@@ -3,4 +3,21 @@ package de.webtwob.input.controller;
 /**
  * Created by BB20101997 on 02. Mär. 2017.
  */
-public class XBox360Const {}
+public class XBox360Const {
+
+    public static final int BUTTON_A  = 0;
+    public static final int BUTTON_B  = 1;
+    public static final int BUTTON_X  = 2;
+    public static final int BUTTON_Y  = 3;
+    public static final int BUTTON_LB  = 4;
+    public static final int BUTTON_RB  = 5;
+    public static final int BUTTON_BACK  = 6;
+    public static final int BUTTON_START  = 7;
+    public static final int BUTTON_LSTICK  = 8;
+    public static final int BUTTON_RSTICK  = 9;
+    public static final int BUTTON_UP = 10;
+    public static final int BUTTON_RIGHT = 11;
+    public static final int BUTTON_DOWN = 12;
+    public static final int BUTTON_LEFT = 13;
+
+}
--- src/de/webtwob/input/controller/XBox360Handler.java
+++ src/de/webtwob/input/controller/XBox360Handler.java
@@ -1,12 +1,64 @@
 package de.webtwob.input.controller;
 
+import de.webtwob.interfaces.IJARLinkable;
+import de.webtwob.interfaces.IJARModel;
+
+import java.nio.ByteBuffer;
+
+import static de.webtwob.input.controller.XBox360Const.*;
+import static org.lwjgl.glfw.GLFW.GLFW_JOYSTICK_1;
+import static org.lwjgl.glfw.GLFW.glfwGetJoystickButtons;
+
 /**
  * Created by BB20101997 on 02. Mär. 2017.
  */
-public class XBoxHandler {
+public class XBox360Handler implements IJARLinkable{
 
-    public static void handleXBoxController(){
+    boolean[] pressed = new boolean[3];
+    ByteBuffer button;
+    IJARModel model;
 
+    public  void handleXBoxController(){
+        button = glfwGetJoystickButtons(GLFW_JOYSTICK_1);
+        if(button.get(BUTTON_A) == 1) {
+            if(model.getMode() == IJARModel.Mode.GAME) {
+                model.jump();
+            } else {
+                if(!pressed[0]) {
+                    pressed[0] = true;
+                    model.doSelect();
+                }
+            }
+        } else {
+            pressed[0] = false;
+        }
+        if(button.get(BUTTON_START) == 1) {
+            model.pause();
+        }
+        if(button.get(BUTTON_DOWN) == 1) {
+            if(!pressed[1]) {
+                pressed[1] = true;
+                model.down();
+            }
+        } else {
+            pressed[1] = false;
+        }
+
+        if(button.get(BUTTON_UP) == 1) {
+            if(!pressed[2]) {
+                pressed[2] = true;
+                model.up();
+            }
+        } else {
+            pressed[2] = false;
+        }
+        if(!model.isSneaking() == (button.get(BUTTON_RB) == 1)) {
+            model.setSneaking(button.get(BUTTON_RB) == 1);
+        }
     }
 
+    @Override
+    public void linkModel(IJARModel ijarm) {
+        model = ijarm;
+    }
 }
