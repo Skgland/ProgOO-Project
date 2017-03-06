bd471d90d461549c6c0a11f9bf8e837302687a4b
BB20101997 <bb-github@t-online.de>
2017-03-06 20:48:41 +0100

    the background color now changes over time

--- src/de/webtwob/view/swing/GameField.java
+++ src/de/webtwob/view/swing/GameField.java
@@ -5,15 +5,19 @@ import de.webtwob.interfaces.IJARModel;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
+import java.util.Random;
 
 /**
- * @author Bennet Blessmann
- *         Created on 05.02.2017.
+ * @author Bennet Blessmann Created on 05.02.2017.
  */
 public class GameField extends Canvas implements Runnable, IJARLinkable {
 
     private BufferStrategy bs;
     private IJARModel      model;
+    private Random rng          = new Random();
+    private Color  currentColor = new Color(rng.nextInt());
+    private Color  nextColor    = new Color(rng.nextInt());
+    private boolean wait;
 
     public GameField() {
         setBackground(Color.BLUE);
@@ -25,6 +29,7 @@ public class GameField extends Canvas implements Runnable, IJARLinkable {
 
         model = ijarm;
     }
+
     @Override
     public void addNotify() {
 
@@ -38,10 +43,11 @@ public class GameField extends Canvas implements Runnable, IJARLinkable {
         super.removeNotify();
         bs = null;
     }
+
     @Override
     public void run() {
 
-        if (model != null && bs != null) {
+        if(model != null && bs != null) {
             //Preparation work
             final double win_width  = getWidth() / 28;
             final double win_height = getHeight() / 14;
@@ -52,14 +58,31 @@ public class GameField extends Canvas implements Runnable, IJARLinkable {
             final double win_player_height = win_height * player_height;
             final double win_player_y      = win_height * player_y;
 
+            final long time = (model.getTime() % 60) - 5;
+
             final Rectangle[] hurdles = model.getHurdles();
 
             //get Graphics every cycle to ensure validity
             final Graphics graphics = bs.getDrawGraphics();
 
+            if(time == 30) {
+                if(!wait) {
+                    wait = true;
+                    currentColor = nextColor;
+                    nextColor = new Color(rng.nextInt());
+                }
+            } else {
+                wait = false;
+            }
+
             //drawBackground
-            graphics.setColor(Color.BLUE);
+            graphics.setColor(currentColor);
             graphics.fillRect(0, 0, getWidth(), getHeight());
+
+            if(time <30) {
+                graphics.setColor(nextColor);
+                graphics.fillRect((int) (getWidth() - time * win_width), 0, (int) (time * win_width), getHeight());
+            }
 
             //drawFlor
             graphics.setColor(Color.GRAY);
@@ -67,32 +90,23 @@ public class GameField extends Canvas implements Runnable, IJARLinkable {
 
             graphics.setColor(Color.CYAN);
             Rectangle rect;
-            for (int i = 0; i < hurdles.length; i++) {
+            for(int i = 0; i < hurdles.length; i++) {
                 rect = hurdles[i];
-                if (rect != null) {
-                    graphics.fillRect(
-                            (int) ((i - 1) * win_width),
-                            (int) (getHeight() - ((rect.getY() + rect.getHeight() + 2) * win_height)),
-                            (int) (rect.getWidth() * win_width),
-                            (int) (rect.getHeight() * win_height)
-                    );
+                if(rect != null) {
+                    graphics.fillRect((int) ((i - 1) * win_width), (int) (getHeight() - ((rect.getY() + rect.getHeight() + 2) * win_height)), (int) (rect.getWidth() * win_width), (int) (rect.getHeight() * win_height));
                 }
             }
 
             //drawPlayer
             graphics.setColor(Color.YELLOW);
-            graphics.fillRect(
-                    (int) win_width,
-                    getHeight() - ((int) (2 * win_height) + (int) (win_player_height) + (int) (win_player_y)),
-                    (int) win_width,
-                    (int) (win_player_height)
-            );
+            graphics.fillRect((int) win_width, getHeight() - ((int) (2 * win_height) + (int) (win_player_height) +
+                                                                      (int) (win_player_y)), (int) win_width, (int) (win_player_height));
 
             graphics.setColor(Color.RED);
             graphics.drawString("Time: " + model.getTime(), 20, 20);
             graphics.drawString("Score: " + model.getScore(), 20, 40);
 
-            if (!bs.contentsLost()) {
+            if(!bs.contentsLost()) {
                 //display buffer
                 bs.show();
             }
