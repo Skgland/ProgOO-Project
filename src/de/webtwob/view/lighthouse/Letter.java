c89cbc20cc1dacf422e4bcef92cfc2d75fe6bf82
BB20101997 <bb-github@t-online.de>
2017-03-06 23:26:42 +0100

    added Text to be displayed on the high rise building

---
+++ src/de/webtwob/view/lighthouse/Letter.java
@@ -0,0 +1,140 @@
+package de.webtwob.view.lighthouse;
+
+/**
+ * Created by BB20101997 on 06. Mär. 2017.
+ */
+public class Letter {
+
+    public static final boolean[][] S = {
+            {true, true, true}, {true, false, false}, {true, true, true}, {false, false, true}, {true, true, true},
+            };
+
+    public static final boolean[][] C = {
+            {true, true, true}, {true, false, false}, {true, false, false}, {true, false, false}, {true, true, true}
+    };
+    public static final boolean[][] O = {
+            {true, true, true}, {true, false, true}, {true, false, true}, {true, false, true}, {true, true, true}
+    };
+    public static final boolean[][] R = {
+            {true, true, false}, {true, false, true}, {true, true, false}, {true, false, true}, {true, false, true}
+    };
+    public static final boolean[][] E = {
+            {true, true, true}, {true, false, false}, {true, true, true}, {true, false, false}, {true, true, true}
+    };
+
+    public static final boolean[][] G = {
+            {false, true, true}, {true, false, false}, {true, true, true}, {true, false, true}, {false, true, true}
+    };
+    public static final boolean[][] A = {
+            {false, true, false}, {true, false, true}, {true, false, true}, {true, true, true}, {true, false, true}
+    };
+
+    public static final boolean[][] M = {
+            {true, false, false, false, true},
+            {true, true, false, true, true},
+            {true, false, true, false, true},
+            {true, false, false, false, true},
+            {true, false, false, false, true}
+    };
+
+    public static final boolean[][] N = {
+            {true, false, false, false, true},
+            {true, true, false, false, true},
+            {true, false, true, false, true},
+            {true, false, false, true, true},
+            {true, false, false, false, true}
+    };
+
+    public static final boolean[][] V = {
+            {true, false, true}, {true, false, true}, {true, false, true}, {false, true, false}, {false, true, false}
+    };
+
+    public static final boolean[][] U = {
+            {true, false, true}, {true, false, true}, {true, false, true}, {true, false, true}, {false, true, false}
+    };
+
+    public static final boolean[][] I = {
+            {true, true, true}, {false, true, false}, {false, true, false}, {false, true, false}, {true, true, true}
+    };
+
+    public static final boolean[][][] SCORE = {S, C, O, R, E};
+    public static final boolean[][][] GAME  = {G, A, M, E};
+    public static final boolean[][][] OVER  = {O, V, E, R};
+    public static final boolean[][][] MENU  = {M, E, N, U};
+    public static final boolean[][][] MAIN  = {M, A, I, N};
+
+    public static final boolean[][][] NUMBERS = {
+            {
+                    {false, true, false},
+                    {true, false, true},
+                    {true, false, true},
+                    {true, false, true},
+                    {false, true, false}
+                    //0
+            }, {
+                    {false, false, true},
+                    {false, true, true},
+                    {true, false, true},
+                    {false, false, true},
+                    {false, false, true}
+                    //1
+            }, {
+                    {false, true, false},
+                    {true, false, true},
+                    {false, false, true},
+                    {false, true, false},
+                    {true, true, true}
+                    //2
+            }, {
+                    {true, true, false},
+                    {false, false, true},
+                    {false, true, false},
+                    {false, false, true},
+                    {true, true, false}
+                    //3
+            }, {
+                    {true, false, true},
+                    {true, false, true},
+                    {true, true, true},
+                    {false, false, true},
+                    {false, false, true}
+                    //4
+            }, {
+                    {true, true, true},
+                    {true, false, false},
+                    {true, true, false},
+                    {false, false, true},
+                    {true, true, false}
+                    //5
+            }, {
+                    {false, true, true},
+                    {true, false, false},
+                    {true, true, false},
+                    {true, false, true},
+                    {false, true, false}
+                    //6
+            }, {
+                    {true, true, true},
+                    {false, false, true},
+                    {false, false, true},
+                    {false, false, true},
+                    {false, false, true}
+                    //7
+            }, {
+                    {false, true, false},
+                    {true, false, true},
+                    {false, true, false},
+                    {true, false, true},
+                    {false, true, false}
+                    //8
+            }, {
+                    {false, true, false},
+                    {true, false, true},
+                    {false, true, true},
+                    {false, false, true},
+                    {true, true, false}
+                    //9
+            }
+
+    };
+}
--- src/de/webtwob/view/lighthouse/LightHouseView.java
+++ src/de/webtwob/view/lighthouse/LightHouseView.java
@@ -43,7 +43,6 @@ public class LightHouseView implements IJARView {
 
     @Override
     public void forceUpdate() {
-
         synchronized(updateLoop) {
             updateLoop.notifyAll();
         }
@@ -73,46 +72,123 @@ public class LightHouseView implements IJARView {
         }
     }
 
+    int i = 0;
+    int n = 0;
+
     private void update() {
 
         if(connect()) {
             while(run) {
                 if(model != null) {
-                    try {//set background
-                        for(byte x = 0; x < 28; x++) {
-                            for(byte y = 0; y < 14; y++) {
-                                setWindowColor(x, y, Color.BLUE);
+                    try {
+
+                        if(model.getMode() == IJARModel.Mode.MENU) {
+                            //space for two lines of max. 7 letters
+                            for(byte x = 0; x < 28; x++) {
+                                for(byte y = 0; y < 14; y++) {
+                                    setWindowColor(x, y, Color.BLUE);
+                                }
                             }
-                        }
 
-                        final byte player_y     = (byte) model.getPlayerY();
-                        final byte player_y_top = (byte) (model.getPlayerY() + model.getPlayerHeight());
+                            switch(model.getCurrentMenu().getText()) {
+                                case "Game Over": {
+                                    long score = model.getScore();
 
-                        //set floor
-                        for(byte x = 0; x < 28; x++) {
-                            setWindowColor(x, (byte) 12, Color.GRAY);
-                        }
+                                    if(score < 100_000) {
 
-                        final Rectangle[] rects = model.getHurdles();
-                        final byte        size  = (byte) rects.length;
-                        for(byte i = 0; i < size && i < 28; i++) {
-                            if(rects[i] != null) {
-                                for(byte x = 0; x < rects[i].getWidth(); x++) {
-                                    for(byte y = 0; y < rects[i].getHeight(); y++) {
-                                        setWindowColor((byte) (i + x), (byte) (11 - rects[i].getY() - y), Color.CYAN);
+                                        byte xoff = 4;
+                                        for(byte i = 0; i < Letter.SCORE.length; i++) {
+                                            setWindowPattern(xoff, (byte) 1, Letter.SCORE[i], Color.RED);
+                                            xoff += Letter.SCORE[i][0].length + 1;
+                                        }
+
+                                        int index = 0;
+                                        int n;
+
+                                        while(score > 0) {
+                                            n = (int) (score % 10);
+                                            setWindowPattern((byte) (20 - 4 * index), (byte) 7, Letter.NUMBERS[n],
+                                                             Color.RED);
+                                            index++;
+                                            score /= 10;
+                                        }
+
+                                    } else {
+                                        //score to high
+                                        byte xoff  = 5;
+                                        byte xoff2 = 6;
+                                        for(int i = 0; i < 4; i++) {
+                                            setWindowPattern(xoff, (byte) 1, Letter.GAME[i], Color.RED);
+                                            setWindowPattern(xoff2, (byte) 7, Letter.OVER[i], Color.RED);
+                                            xoff += Letter.GAME[i][0].length + 1;
+                                            xoff2 += Letter.OVER[i][0].length + 1;
+                                        }
+                                    }
+                                    break;
+                                }
+                                case "Main Menu": {
+                                    byte xoff = 4;
+                                    for(byte i = 0; i < Letter.MAIN.length; i++) {
+                                        setWindowPattern(xoff, (byte) 1, Letter.MAIN[i], Color.RED);
+                                        xoff += Letter.MAIN[i][0].length + 1;
+                                    }
+
+                                    xoff = 4;
+                                    for(byte i = 0; i < Letter.MENU.length; i++) {
+                                        setWindowPattern(xoff, (byte) 7, Letter.MENU[i], Color.RED);
+                                        xoff += Letter.MENU[i][0].length + 1;
+                                    }
+                                    break;
+                                }
+                                default: {
+                                    byte xoff = 4;
+                                    for(byte i = 0; i < Letter.MENU.length; i++) {
+                                        setWindowPattern(xoff, (byte) 1, Letter.MENU[i], Color.RED);
+                                        xoff += Letter.MENU[i][0].length + 1;
                                     }
                                 }
                             }
-                        }
+                            send(windows);
+                            synchronized(updateLoop) {
+                                updateLoop.wait(10);
+                            }
+                        } else {
+                            //set background
+                            for(byte x = 0; x < 28; x++) {
+                                for(byte y = 0; y < 14; y++) {
+                                    setWindowColor(x, y, Color.BLUE);
+                                }
+                            }
 
-                        //paint player
-                        for(byte y = player_y; y < player_y_top; y++) {
-                            setWindowColor((byte) 1, (byte) (11 - y), Color.YELLOW);
-                        }
+                            final byte player_y     = (byte) model.getPlayerY();
+                            final byte player_y_top = (byte) (model.getPlayerY() + model.getPlayerHeight());
 
-                        send(windows);
-                        synchronized(updateLoop) {
-                            updateLoop.wait(10);
+                            //set floor
+                            for(byte x = 0; x < 28; x++) {
+                                setWindowColor(x, (byte) 12, Color.GRAY);
+                            }
+
+                            final Rectangle[] rects = model.getHurdles();
+                            final byte        size  = (byte) rects.length;
+                            for(byte i = 0; i < size && i < 28; i++) {
+                                if(rects[i] != null) {
+                                    for(byte x = 0; x < rects[i].getWidth(); x++) {
+                                        for(byte y = 0; y < rects[i].getHeight(); y++) {
+                                            setWindowColor((byte) (i + x), (byte) (11 - rects[i].getY() - y), Color.CYAN);
+                                        }
+                                    }
+                                }
+                            }
+
+                            //paint player
+                            for(byte y = player_y; y < player_y_top; y++) {
+                                setWindowColor((byte) 1, (byte) (11 - y), Color.YELLOW);
+                            }
+
+                            send(windows);
+                            synchronized(updateLoop) {
+                                updateLoop.wait(10);
+                            }
                         }
                     }
                     catch(final NullPointerException | InterruptedException ignore) {
@@ -136,13 +212,30 @@ public class LightHouseView implements IJARView {
     }
 
     private void setWindowColor(final byte x, final byte y, final Color color) {
+        final int index = coordToWinNumber(x, y);
 
-        assert x >= 0 && x < 28 : "Window X-Coordinate ot of bounds was:" + x;
-        assert y >= 0 && y < 14 : "Window Y-Coordinate ot of bounds was:" + y;
+        if(index + 2 >= windows.length) {
+            return;
+        }
 
-        windows[coordToWinNumber(x, y)] = (byte) color.getRed();
-        windows[coordToWinNumber(x, y) + 1] = (byte) color.getGreen();
-        windows[coordToWinNumber(x, y) + 2] = (byte) color.getBlue();
+        windows[index] = (byte) color.getRed();
+        windows[index + 1] = (byte) color.getGreen();
+        windows[index + 2] = (byte) color.getBlue();
+    }
+
+    /**
+     * @param x       x-offset
+     * @param y       y-offset
+     * @param pattern not as may be intuitive [x][y] instead [y][x]
+     */
+    private void setWindowPattern(final byte x, final byte y, final boolean[][] pattern, Color c) {
+        for(byte iy = 0; iy < pattern.length; iy++) {
+            for(byte ix = 0; ix < pattern[iy].length; ix++) {
+                if(pattern[iy][ix]) {
+                    setWindowColor((byte) (x + ix), (byte) (y + iy), c);
+                }
+            }
+        }
     }
 
     private short coordToWinNumber(final byte x, final byte y) {
