06d757f2a65b87e139eb3c6ca580c559b80e616c
BB20101997 <bb-github@t-online.de>
2017-03-06 20:43:49 +0100

    started working on the ~50 line comment

---
+++ src/main/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * This package contains the Main class which contains the main method
+ * that starts this program.
+ *
+ *  Now following is the 50 line comment or so about our usage of MVC and decomposition.
+ *
+ *  The program has several components.
+ *  All components that are (part of) a Controller can be found in the de.webtwob.input package
+ *  All components that are (part of) a View can be found in the de.webtwob.view package
+ *      The view that implements the Lighthouse API can be found in the de.webtwob.view.lighthouse package
+ *  All components that are (part of) a Model can be found in the de.webtwob.model package
+ *
+ *  All interfaces can be found in the package de.webtwob.interfaces
+ *
+ *
+ * */
+package main;
