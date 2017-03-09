b98b644cec8f475ef8d07e4942ebbd79ae068e31
Bennet <bb-github@t-online.de>
2017-03-09 12:16:04 +0100

    removed linkable from view

--- src/de/webtwob/interfaces/IJARView.java
+++ src/de/webtwob/interfaces/IJARView.java
@@ -4,7 +4,7 @@ package de.webtwob.interfaces;
  * @author Bennet Blessmann
  * Created on 31. Jan. 2017.
  */
-public interface IJARView extends IJARLinkable, IJARRunable{
+public interface IJARView extends IJARRunable{
 
     /**
      * causes the view to update all components
