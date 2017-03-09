aa6b9ac8a03576b279efe6eec14bd425f3760d15
Yannik Sembritzki <yannik@sembritzki.me>
2017-03-09 12:13:14 +0100

    Refactor UpAction

--- src/de/webtwob/input/action/UpAction.java
+++ src/de/webtwob/input/action/UpAction.java
@@ -1,17 +1,29 @@
 package de.webtwob.input.action;
 
+import de.webtwob.interfaces.IJARMenuModel;
+import de.webtwob.interfaces.Mode;
+import de.webtwob.model.menu.ModeModel;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 /**
  * @author Bennet Blessmann
  * Created on 31. Jan. 2017.
  */
-public class UpAction extends LinkableAction {
+public class UpAction extends AbstractAction {
 
+    private final ModeModel modeModel;
+    private final IJARMenuModel menuModel;
+
+    public UpAction(ModeModel modeModel, IJARMenuModel menuMode) {
+        this.modeModel = modeModel;
+        this.menuModel = menuMode;
+    }
     @Override
     public void actionPerformed(final ActionEvent e) {
-        if(model.getMode() == IJARModel.Mode.MENU) {
-            model.up();
+        if(modeModel.getMode() == Mode.MENU) {
+            menuModel.up();
         }
     }
 
